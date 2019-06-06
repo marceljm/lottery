81731825a2be9715c044aa9c7fc0aeddae367f15
Marcel Jacques Machado <marceljmachado@gmail.com>
2019-06-06 19:40:08 -0300

    Improvements

--- data/getDataset.sh
+++ data/getDataset.sh
@@ -1,9 +1,9 @@
 wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip
 unzip D_lotfac.zip
 rm D_lotfac.zip
-rm LOTFACIL.GIF
-lynx --dump D_LOTFAC.HTM > tmp
-rm D_LOTFAC.HTM
+rm lotfacil.gif 
+lynx --dump d_lotfac.htm > tmp
+rm d_lotfac.htm
 echo "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O" > dataset.csv
 egrep '[0-9]{2}/[0-9]{2}/..' tmp | sed -e 's/   //' | cut -d' ' -f3-17 | tr ' ' ',' >> dataset.csv
 rm tmp
